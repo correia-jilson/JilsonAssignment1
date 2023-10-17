89bb3424d81c781cc5f48401260d8281323950a5
Jilson Correia <correia.ji@northeastern.edu>
2023-10-17 19:10:42 -0400

    first

--- assignment-1-shopping-correia-jilson/src/main/java/edu/neu/csye6200/Cart.java
+++ assignment-1-shopping-correia-jilson/src/main/java/edu/neu/csye6200/Cart.java
@@ -3,14 +3,15 @@ package edu.neu.csye6200;
 public class Cart {
 	
 	private double myCash;
-    private double myTotal;
+    
+	private double myTotal;
     private double myChange;
     
     public Cart(double cash) {
     	
     	myCash = cash;
-    	myTotal = 0;
-    	myChange = 0;
+    	myTotal= 0.0;
+    	myChange = 0.0;
     	
     }
     
@@ -26,29 +27,55 @@ public class Cart {
     	return myTotal;
     }
     
+    public void setMyCash(double myCash) {
+		this.myCash = myCash;
+	}
+
+	public void setMyTotal(double myTotal) {
+		this.myTotal = myTotal;
+	}
+
+	public void setMyChange(double myChange) {
+		this.myChange = myChange;
+	}
+
+    
     
     
 
-	public void sillyCheckout(double price) {
-        double remainingChange = myCash - myTotal - price;
-        if (remainingChange >= 0) {
-            myTotal += price;
-            myChange += remainingChange;
+    public void sillyCheckout(double cash,double price,double total, double change) {
+        double tempCash = cash;
+        double tempTotal = total;
+        double tempChange = change;
+
+        if (tempCash >= price) {
+            tempTotal = tempTotal + price;
+            tempChange = tempCash - price;
+            tempCash = tempCash - tempTotal;
         }
-		
+        
+        System.out.println("Item Price: $" + price);
+        System.out.println("Total: $" + tempTotal + ", Change: $" + String.format("%.2f", tempChange));
+
     }
 
+    
+    
     
     public double checkout(Cart myCart, Item myItem) {
-        double change = myCart.getMyCash() - myCart.getMyTotal() - myItem.getItemCost();
-        if (change >= 0) {
-            myCart.myTotal += myItem.getItemCost();
-            myCart.myChange += change;
+
+        if (myCart.getMyCash() >= myItem.getItemCost()) {
+            myCart.setMyTotal(myCart.getMyTotal() + myItem.getItemCost());
+            myCart.setMyChange(myCart.getMyCash() - myItem.getItemCost());
+            myCart.setMyCash(getMyChange());
         }
-        return change;
-    }
+        
+        System.out.println("Item Name: $" + myItem.getItemId() +"Item Price: $" + myItem.getItemCost());
+
+      return myCart.getMyChange();
+  }
 
     public String toString() {
-        return "Cash: $" + myCash + ", Total: $" + myTotal + ", Change: $" + myChange;
+        return "Cash: $" + myCash + ", Total: $" + myTotal + ", Change: $" +String.format("%.2f", myChange);
     }
 }
--- assignment-1-shopping-correia-jilson/src/main/java/edu/neu/csye6200/Item.java
+++ assignment-1-shopping-correia-jilson/src/main/java/edu/neu/csye6200/Item.java
@@ -26,27 +26,28 @@ public class Item {
 		
 		Date dateTime = new Date();
 		
-        Cart myCart = new Cart(20); // Initialize Cart with $20
+        Cart myCart = new Cart(20.00); // Initialize Cart with $20
         Item item1 = new Item("Pizza" , 10.99);
         Item item2 = new Item("Bread Sticks" , 4.99);
         Item item3 = new Item("Candy" , 1.99);
 
-        double change1 = myCart.sillyCheckout(myCart.getMyCash(), item1.getItemCost(), myCart.getMyTotal(), myCart.getMyChange());
-        double change2 = myCart.sillyCheckout(myCart.getMyCash(), item2.getItemCost(), myCart.getMyTotal(), myCart.getMyChange());
-        double change3 = myCart.sillyCheckout(myCart.getMyCash(), item3.getItemCost(), myCart.getMyTotal(), myCart.getMyChange());
-        
+        myCart.sillyCheckout(myCart.getMyCash(), item1.getItemCost(), myCart.getMyTotal(), myCart.getMyChange());
+		myCart.sillyCheckout(myCart.getMyCash(), item2.getItemCost(), myCart.getMyTotal(), myCart.getMyChange());
+		myCart.sillyCheckout(myCart.getMyCash(), item3.getItemCost(), myCart.getMyTotal(), myCart.getMyChange());
+		
         
         System.out.println("______________________________");
 		System.out.println("\n*******Reciept*******\n");
 		
         System.out.println("Date & Time : " +dateTime);
         
-        System.out.println(item1.getItemId() + ": $" + item1.getItemCost());
-        System.out.println(item2.getItemId() + ": $" + item2.getItemCost());
-        System.out.println(item3.getItemId() + ": $" + item3.getItemCost() + " - Change: $" + change3);
+        myCart.checkout(myCart, item1);
+        myCart.checkout(myCart, item2);
+        myCart.checkout(myCart, item3); 
         
         System.out.println("Total: $" + 20 );
-        System.out.println("Remaining Change: $" + myCart.getMyChange()+"\n");
+        System.out.println("Remaining Change: $");
+        System.out.println(myCart);
         System.out.println("\nThank You for Shopping\n\nDo Visit Again");
         System.out.println("______________________________");
 		
